9b6fc2b4916f1ae61637c3b270a208838a48b05c
AT <85502327+Adarsh-Game-Changer@users.noreply.github.com>
2022-06-07 12:30:23 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyK9Or4rNoz66J55dK8KpIkv4d_wXv7DY",
+  authDomain: "proooooooooo-c71.firebaseapp.com",
+  projectId: "proooooooooo-c71",
+  storageBucket: "proooooooooo-c71.appspot.com",
+  messagingSenderId: "318559544959",
+  appId: "1:318559544959:web:b594013f906b89af1c814a"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
